9602c2c41e3f5c53a0d7d451145b7f03f7e6b861
JB van der Merwe <jb@jbkamp.com>
2022-05-14 17:34:13 +0200

    Moved most homepage code (view and viewmodel) into seperate, reusable, widgets.

--- lib/ui/screens/homepage_view.dart
+++ lib/ui/screens/homepage_view.dart
@@ -1,4 +1,8 @@
+import 'package:aura_web_dashboard/models/panic_model.dart';
+import 'package:aura_web_dashboard/ui/widgets/panics/panics_view.dart';
+import 'package:aura_web_dashboard/ui/widgets/users/users_view.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:stacked/stacked.dart';
 import 'package:url_launcher/link.dart';
 
@@ -7,60 +11,22 @@ import 'homepage_viewmodel.dart';
 class HomePageView extends StatelessWidget {
   HomePageView({Key? key}) : super(key: key);
 
+  final Logger _logger = Logger();
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<HomePageViewModel>.reactive(
       builder: (BuildContext context, HomePageViewModel model, Widget? child) {
         return Scaffold(
-          body: model.isBusy ? CircularProgressIndicator() : model.hasError ?  Center(child: Text("ERROR"),) :
-          ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  children: [
-                    Expanded(child: Text(model.panicModel!.panics[index].username!)),
-                    Expanded(child: Text(model.panicModel!.panics[index].mobile!)),
-                    Expanded(child: Text(model.panicModel!.panics[index].address!)),
-                    Expanded(child: Text("${model.panicModel!.panics[index].timestamp!}")),
-                    Expanded(child: Text(model.panicModel!.panics[index].panictypename!)),
-                    Expanded(child: Text("${model.panicModel!.panics[index].lat!}, ${model.panicModel!.panics[index].long!}")),
-                    Link(
-                      target: LinkTarget.blank,
-                      uri: Uri.parse("https://www.google.com/maps/@${model.panicModel!.panics[index].lat!},${model.panicModel!.panics[index].long!},17z,"),
-                      builder: (_, dynamic followLink) {
-                        return TextButton(onPressed: followLink, child: Text("Map"));
-                      },
-                    )
-                  ],
-                ),
-              );
-            },
-            itemCount: model.panicModel == null ? 0 : model.panicModel!.panics.length,
-          )
-        ) ;
+          body: Column(
+            children: [
+              PanicsView(),
+              UsersView()
+            ],
+          ),
+        );
       },
       viewModelBuilder: () => HomePageViewModel(),
-    );
-  }
-
-  Widget _dropdownButton(HomePageViewModel model) {
-    return
-     DropdownButton(
-      value: model.dropdownvalue,
-        
-      icon: const Icon(Icons.keyboard_arrow_down),    
-        
-      items: model.items.map((String items) {
-        return DropdownMenuItem(
-          value: items,
-          child: Text(items),
-        );
-      }).toList(),
-      onChanged: (String? newValue) {
-        model.setDropdownValue(newValue);
-        model.dropdownvalue = newValue!;
-      },
     );
   }
 }
--- lib/ui/screens/homepage_viewmodel.dart
+++ lib/ui/screens/homepage_viewmodel.dart
@@ -1,34 +1,5 @@
-import 'package:aura_web_dashboard/app/locator.dart';
-import 'package:aura_web_dashboard/models/panic_model.dart';
-import 'package:aura_web_dashboard/services/api_service.dart';
-import 'package:logger/logger.dart';
 import 'package:stacked/stacked.dart';
 
-class HomePageViewModel extends FutureViewModel {
+class HomePageViewModel extends BaseViewModel {
 
-
-  final APIService  _apiService = getIt.get<APIService>();
-  final Logger      _logger     = Logger();
-  PanicModel?       _panicModel;
-
-  PanicModel? get panicModel => _panicModel;
-
-    List<String> items = [];
-
-  String dropdownvalue = '';   
-
-  void setDropdownValue(String? val) async {
-    dropdownvalue = val!;
-    notifyListeners();
-  }
-  
-  @override
-  Future futureToRun() async {
-    _panicModel = await _apiService.getPanics();
-  }
-  
-  @override
-  void onError(error) {
-    _logger.e(error);
-  }
 }
